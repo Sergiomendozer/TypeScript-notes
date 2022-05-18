429c02f26af8c3d03aefc580fafe117a88d526f6
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-18 14:58:02 -0700

    functions is TypeScript

---
+++ aliases.js
@@ -0,0 +1,10 @@
+"use strict";
+const carYear = 2001;
+const carType = "Toyota";
+const carModel = "Corolla";
+const cars = {
+    year: carYear,
+    type: carType,
+    model: carModel,
+};
+console.log(cars);
